4f6c0d1c9b253b07423ac76f06099f5c612c61bb
diljeet <diljeet@localhost.localdomain>
2020-05-31 21:59:45 +0530

    initial commit

---
+++ src/main/java/com/diljeet/onetomany/web/OnetomanyController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.diljeet.onetomany.web;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author diljeet
+ */
+@Named(value = "onetomanyController")
+@RequestScoped
+public class OnetomanyController {
+
+    /**
+     * Creates a new instance of onetomanyController
+     */
+    public OnetomanyController() {
+    }
+    
+}
